f4cddc78281227d65a175d888b209d599937c473
eusoubrian <brian.gomes@sptech.school>
2023-10-07 00:43:18 -0300

    Criando funções para editar a input na area de controle

--- site/public/js/cadastroADM.js
+++ site/public/js/cadastroADM.js
@@ -11,6 +11,86 @@ function closeModal(){
     modalAdm.close()
 }
 
+function testeAdm() {
+    modalAdm.close()
+    Swal.fire(
+        'Admin Cadastrado com sucesso!',
+        'Adicionando...',
+        'success'
+      )
+}
+
+function excluirAdm() {
+    Swal.fire({
+        title: 'Deletar Admin',
+        text: "Certeza que deseja deletar esse Admin?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Deletado!',
+            'Usuário Admin deletado com sucesso!',
+            'success'
+          )
+        }
+      })
+}
+
+function habilitarEdicao(button) {
+    var row = button.parentNode.parentNode;
+    var inputs = row.getElementsByTagName('input');
+
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].removeAttribute('readonly');
+        inputs[i].style.pointerEvents = 'all';
+
+        // Adiciona eventos de teclado e foco para cada input
+        inputs[i].addEventListener('keydown', function(event) {
+            // Se a tecla Enter for pressionada
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                confirmarAlteracao(this);
+            }
+        });
+
+        inputs[i].addEventListener('blur', function() {
+            confirmarAlteracao(this);
+        });
+    }
+}
+
+function confirmarAlteracao(input) {
+    // Exibe um alerta ou usa SweetAlert2 para confirmar a alteração
+    Swal.fire({
+        title: 'Confirmar Alteração',
+        text: 'Deseja realmente fazer essa alteração?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Aqui você pode implementar o código para atualizar o banco de dados
+            input.setAttribute('readonly', 'readonly');
+            input.style.pointerEvents = 'none';
+        } else {
+            // Reverta as alterações se o usuário cancelar
+            input.value = input.defaultValue;
+        }
+
+        // Desabilita o pointer-events para impedir a interação com todas as inputs
+        var allInputs = document.getElementsByTagName('input');
+        for (var i = 0; i < allInputs.length; i++) {
+            allInputs[i].style.pointerEvents = 'none';
+        }
+    });
+}
+
 function CadastrarAdm(){
     var nomeAdmVar = nomeAdm.value;
     var areaVar = area.value;
