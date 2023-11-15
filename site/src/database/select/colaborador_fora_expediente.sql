ce1eff4f3f8eefce8bcc560f50aae18ee5ebceb0
eusoubrian <brian.gomes@sptech.school>
2023-11-14 21:59:23 -0300

    select colaborador fora expediente

--- site/src/database/select/colaborador_fora_expediente.sql
+++ site/src/database/select/colaborador_fora_expediente.sql
@@ -3,28 +3,24 @@ WITH DiferencaCapturas AS (
         id_maquina,
         usuario.nome as nome_usuario,
         id_usuario,
-        DATE(data_captura) AS data_captura_dia,
+        MAX(data_captura) as ultima_captura_dia,
         MIN(data_captura) AS primeira_captura_dia,
-        MAX(data_captura) AS ultima_captura_dia,
         desligada,
         CASE
-            WHEN HOUR(MIN(data_captura)) < 9 OR HOUR(MAX(data_captura)) > 17 THEN true
+            WHEN HOUR(MIN(data_captura)) < 7 OR HOUR(MAX(data_captura)) > 19 THEN true
             ELSE false
         END AS aviso
     FROM dados_captura
     JOIN componente ON dados_captura.fk_componente = componente.id_componente
     JOIN maquina ON componente.fk_maquina = maquina.id_maquina
     JOIN usuario ON maquina.fk_usuario = usuario.id_usuario
-    GROUP BY id_maquina, nome_usuario, id_usuario, data_captura_dia, desligada
+    WHERE DATE(data_captura) = CURDATE() -- Considera apenas os dados do dia de hoje
+    GROUP BY id_maquina, nome_usuario, id_usuario, desligada
 )
 SELECT
     id_maquina,
     nome_usuario,
-    MAX(ultima_captura_dia) AS ultima_captura,
-    MIN(primeira_captura_dia) AS primeira_captura,
-    TIMESTAMPDIFF(MINUTE, MIN(primeira_captura_dia), MAX(ultima_captura_dia)) AS tempo_atividade_minutos,
-    MAX(aviso) AS aviso
+    SUM(CASE WHEN aviso THEN TIMESTAMPDIFF(MINUTE, primeira_captura_dia, ultima_captura_dia) ELSE 0 END) AS tempo_fora_expediente_minutos
 FROM DiferencaCapturas
-WHERE desligada = 0
-AND id_usuario = 1
-GROUP BY id_maquina, nome_usuario;
+GROUP BY id_maquina, nome_usuario
+ORDER BY tempo_fora_expediente_minutos DESC;
