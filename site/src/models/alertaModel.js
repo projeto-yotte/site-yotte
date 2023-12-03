036c19d87c2e8c184c1afd1a22b47b3aaedf33bb
JuliaHown <juliahown87@gmail.com>
2023-12-02 22:25:33 -0300

    Correção do select

--- site/src/models/alertaModel.js
+++ site/src/models/alertaModel.js
@@ -5,27 +5,27 @@ function listar() {
     if (process.env.AMBIENTE_PROCESSO == "producao") {
         var instrucao = `
         SELECT
-        funcionario.nome AS nome,
-        descricao,
-        ip
-    FROM
-        usuario AS admin
-    JOIN
-        token ON admin.id_usuario = token.fk_usuario
-    JOIN
-        maquina ON token.idtoken = maquina.fk_token
-    JOIN
-        usuario AS funcionario ON funcionario.id_usuario = maquina.fk_usuario
-    JOIN
-        dados_captura ON dados_captura.fk_componente = maquina.fk_componente
-    JOIN
-        alerta ON dados_captura.id_dados_captura = alerta.fk_dados_captura
-    JOIN
-        empresa ON funcionario.fk_empresa = empresa.id_empresa
-    WHERE
-        admin.id_usuario = 1 AND empresa.id_empresa = 1;
-    
-                    `;
+    funcionario.nome AS nome,
+    alerta.descricao,
+    maquina.ip
+FROM
+    usuario AS admin
+JOIN
+    token ON admin.id_usuario = token.fk_usuario
+JOIN
+    maquina ON token.idtoken = maquina.fk_token
+JOIN
+    usuario AS funcionario ON funcionario.id_usuario = maquina.fk_usuario
+JOIN
+    componente ON maquina.id_maquina = componente.fk_maquina
+JOIN
+    dados_captura ON componente.id_componente = dados_captura.fk_componente
+JOIN
+    alerta ON dados_captura.id_dados_captura = alerta.fk_dados_captura
+JOIN
+    empresa ON funcionario.fk_empresa = empresa.id_empresa
+WHERE
+    admin.id_usuario = ${id_admin}  AND empresa.id_empresa = ${id_empresa};`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         var instrucao = `
             SELECT
