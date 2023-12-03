1e51d6050c1aba231e86b09b83a362462173273b
JuliaHown <juliahown87@gmail.com>
2023-12-02 21:47:59 -0300

    Configurando as credencias do SQL Server

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -3,10 +3,10 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    server: "54.205.98.102",
+    database: "yotte",
+    user: "sa",
+    password: "Projetoyotte2023",
     pool: {
         max: 10,
         min: 0,
